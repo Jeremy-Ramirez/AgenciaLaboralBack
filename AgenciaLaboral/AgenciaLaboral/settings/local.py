f204e5470403bb5dd0361a0e872decf42d8fb0ec
diacrami <diacrami@espol.edu.ec>
2021-10-12 13:58:12 -0500

    cambio

--- AgenciaLaboral/AgenciaLaboral/settings/local.py
+++ AgenciaLaboral/AgenciaLaboral/settings/local.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'agenciadb',
         'USER':'root',
-        'PASSWORD':'Bakugo_23y24'
+        'PASSWORD':'admin'
     }
 }
 
