14a969a8d7c52cddc2e4f856aea2a1e1349dcf10
diacrami <diacrami@espol.edu.ec>
2021-10-29 20:27:10 +0000

    corrección tamaño sugerencia

--- AgenciaLaboral/applications/Sugerencia/models.py
+++ AgenciaLaboral/applications/Sugerencia/models.py
@@ -11,10 +11,10 @@ def nameFile(instance, filename):
 class Sugerencia(models.Model):
     idsugerencia = models.AutoField(db_column='idSugerencia', primary_key=True)  # Field name made lowercase.    titulo = models.CharField(max_length=45, blank=True, null=True)
     titulo = models.CharField(max_length=45, blank=True, null=True)
-    descripcion = models.CharField(max_length=45, blank=True, null=True)
+    descripcion = models.CharField(max_length=600, blank=True, null=True)
     imagen = models.ImageField(blank=True, upload_to=nameFile, null=True)
     usuario_idusuario = models.ForeignKey(Usuario, models.DO_NOTHING, db_column='Usuario_idUsuario')  # Field name made lowercase.
 
     class Meta:
-        
+
         db_table = 'sugerencia'
