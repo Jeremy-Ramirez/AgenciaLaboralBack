527a98c02dda15dbd284d37b4d872bf592643afa
diacrami <diacrami@espol.edu.ec>
2021-10-29 20:24:55 +0000

    rutas y correcciones

--- AgenciaLaboral/applications/Aspirante/models.py
+++ AgenciaLaboral/applications/Aspirante/models.py
@@ -15,14 +15,14 @@ class Aspirante(models.Model):
     videopresentacion = models.FileField(blank=True, upload_to=nameFile, null=True)  # Field name made lowercase.
     aniosexperiencia = models.IntegerField(db_column='aniosExperiencia', blank=True, null=True)  # Field name made lowercase.
     fechanacimiento = models.DateField(db_column='fechaNacimiento', blank=True, null=True)  # Field name made lowercase.
-    posibilidadviajar = models.CharField(db_column='posibilidadViajar', blank=True, null=True)  # Field name made lowercase.
+    posibilidadviajar = models.CharField(db_column='posibilidadViajar', max_length=100, blank=True, null=True)  # Field name made lowercase.
     profesiones_idprofesiones = models.ForeignKey(Profesiones, models.DO_NOTHING, db_column='Profesiones_idProfesiones')  # Field name made lowercase.
     usuario_idusuario = models.ForeignKey(Usuario, models.DO_NOTHING, db_column='usuario_idusuario',null=True) # Field name made lowercase.
-    
+
     def __str__(self):
         txt = " id: {0} / Experiencia: {1} / Campo: {2} "
-        return txt.format(self.idaspirante , self.experiencialaboral , self.campolaboral ) 
+        return txt.format(self.idaspirante , self.experiencialaboral , self.campolaboral )
 
     class Meta:
-        
+
         db_table = 'aspirante'
