2be92df8fc5186715534b26018ce0112f225bde6
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2021-12-20 14:22:24 -0500

    nuevo front

--- AgenciaLaboral/home/urls.py
+++ AgenciaLaboral/home/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('aspirante/registroFullAspirante', views.index),
     path('empresa/registroEmpresa', views.index),
     path('empresa/registroFullEmpresa', views.index),
-    path('administrador/registroAdministrador', views.index),
+
 
     path('aspirante/sesionAspirante', views.index),
     path('aspirante/sesionAspirante/perfilAspirante', views.index),
@@ -28,37 +28,23 @@ urlpatterns = [
     path('empresa/sesionEmpresa/registroRepresentante', views.index),
     path('empresa/sesionEmpresa/perfilEmpresa', views.index),
     path('empresa/sesionEmpresa/sugerencias', views.index),
-    path('empresa/sesionEmpresa/editpass', views.index),
 
 
     path('administrador/sesionAdministrador', views.index),
-    path('administrador/sesionAdministrador/aspirantesAceptados', views.index),
     path('administrador/sesionAdministrador/aspirantesNuevos', views.index),
-    path('administrador/sesionAdministrador/asignarAspirante', views.index
-    path('administrador/sesionAdministrador/perfilAdministrador', views.index),
+    path('administrador/sesionAdministrador/aspirantesAceptados', views.index),
     path('administrador/sesionAdministrador/perfilAspirante', views.index),
-    path('administrador/sesionAdministrador/perfilEmpresa', views.index),
-    path('administrador/sesionAdministrador/empresas', views.index),
-    path('administrador/sesionAdministrador/revisarempresas', views.index),
-    path('administrador/sesionAdministrador/sugerencias', views.index),
-    path('administrador/sesionAdministrador/autorizarEmpresas', views.index),
-    path('administrador/sesionAdministrador/paquetePago', views.index),
-
     #path(r'^administrador/sesionAdministrador/perfilAspirante/\d+/$', views.index, name='perfil'),
-
-
+    path('administrador/sesionAdministrador/empresas', views.index),
+    path('administrador/sesionAdministrador/sugerencias', views.index),
 
 
     path('representante/sesionRepresentante', views.index),
     path('representante/sesionRepresentante/registroFullRepresentante', views.index),
     path('representante/sesionRepresentante/perfilRepresentante', views.index),
-    path('representante/sesionRepresentante/sugerencias', views.index),
-    path('representante/sesionRepresentante/solicitudesRepresentante', views.index),
     path('representante/sesionRepresentante/showSolicitudes', views.index),
-    path('representante/sesionRepresentante/filtroAspirantes', views.index),
-    path('representante/sesionRepresentante/consultarSolicitudes', views.index),
-
-
+    path('representante/sesionRepresentante/solicitudesRepresentante', views.index),
+    path('representante/sesionRepresentante/sugerencias', views.index),
 
 
 ]
