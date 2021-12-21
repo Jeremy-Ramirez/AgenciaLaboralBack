8bdd9e75f8e2a21218d3f7296117d307984adbda
diacrami <diacrami@espol.edu.ec>
2021-12-21 02:11:19 +0000

    rutas administrador

--- AgenciaLaboral/home/urls.py
+++ AgenciaLaboral/home/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('aspirante/registroFullAspirante', views.index),
     path('empresa/registroEmpresa', views.index),
     path('empresa/registroFullEmpresa', views.index),
+    path('empresa/registroAdministrador', views.index),
+
 
 
     path('aspirante/sesionAspirante', views.index),
@@ -31,12 +33,19 @@ urlpatterns = [
 
 
     path('administrador/sesionAdministrador', views.index),
-    path('administrador/sesionAdministrador/aspirantesNuevos', views.index),
     path('administrador/sesionAdministrador/aspirantesAceptados', views.index),
+    path('administrador/sesionAdministrador/aspirantesNuevos', views.index),
+    path('administrador/sesionAdministrador/asignarAspirante', views.index),
+    path('administrador/sesionAdministrador/perfilAdministrador', views.index),
     path('administrador/sesionAdministrador/perfilAspirante', views.index),
     #path(r'^administrador/sesionAdministrador/perfilAspirante/\d+/$', views.index, name='perfil'),
+    path('administrador/sesionAdministrador/perfilEmpresa', views.index),
     path('administrador/sesionAdministrador/empresas', views.index),
+    path('administrador/sesionAdministrador/revisarempresas', views.index),
     path('administrador/sesionAdministrador/sugerencias', views.index),
+    path('administrador/sesionAdministrador/autorizarEmpresas', views.index),
+    path('administrador/sesionAdministrador/paquetePago', views.index),
+
 
 
     path('representante/sesionRepresentante', views.index),
